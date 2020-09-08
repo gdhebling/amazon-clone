1965f15c4f0702442883b4ffe6cafab7a0aa0de2
Gui Hebling <guilherme.durso@outlook.com>
2020-09-08 21:56:42 +0200

    Create and import Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>Hello Clever Programmers, let's build the Amazon Store 🚀</h1>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
