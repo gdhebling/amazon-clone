0793b53427518a17711f6f705804aa09db4a24df
Gui Hebling <guilherme.durso@outlook.com>
2020-09-11 15:30:43 +0200

    Finished Remove from Basket functionality

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import "./CheckoutProduct.css"
+import { useStateValue } from './StateProvider';
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+
+    const [{ basket }, dispatch] = useStateValue();
+
+    const removeFromBasket = () => {
+        // remove the item from the basket
+        dispatch({
+            type: "REMOVE_FROM_BASKET",
+            id: id,
+        })
+    }
+
     return (
         <div className="checkoutProduct">
             <img src={image} alt="" className="checkoutProduct__image" />
 
             <div className="checkoutProduct__info">
-                <p className="checkoutProduct__title">{title}
+                <p className="checkoutProduct__title">{title}</p>
+                <p className="checkoutProduct__price">
                     <small>$</small>
                     <strong>{price}</strong>
                 </p>
@@ -16,7 +29,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
                         <span role="img" aria-label="star">⭐</span>
                     ))}
                 </div>
-                <button>Remove from Basket</button>
+                <button onClick={removeFromBasket}>Remove from Basket</button>
             </div>
         </div>
     )
