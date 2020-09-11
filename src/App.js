f1b05cdbbd95646cfb0144b7210c5d28b01c71de
Gui Hebling <guilherme.durso@outlook.com>
2020-09-11 16:30:16 +0200

    Built Front-End of Login Page

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Checkout from './Checkout';
+import Login from './Login';
 
 function App() {
   return (
     <Router>
       <div className="app">
 
-        <Header />
-
         <Switch>
 
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
 
           {/* Default Route at the bottom:  */}
           <Route path="/">
+            <Header />
             <Home />
           </Route>
 
