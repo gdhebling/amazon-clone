2ef8edc8351da622625530643a68ef306906a0a6
Gui Hebling <guilherme.durso@outlook.com>
2020-09-11 16:39:21 +0200

    On Login, added buttons onChange and onClick properties

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
 import { Link } from 'react-router-dom'
 
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault()
+
+        // firebase login here
+    }
+
+    const register = e => {
+        e.preventDefault()
+
+        // firebase register here 
+    }
+
     return (
         <div className="login">
             <Link to="/">
@@ -13,19 +29,19 @@ function Login() {
                 <h1>Sign In</h1>
                 <form action="">
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button className="login__signInButton">Sign In</button>
+                    <button type="submit" className="login__signInButton" onClick={signIn}>Sign In</button>
                 </form>
 
                 <p>
                     By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
 
-                <button className="login__registerButton">Create your Amazon Account</button>
+                <button className="login__registerButton" onClick={register}>Create your Amazon Account</button>
             </div>
         </div>
     )
