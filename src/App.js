d3d2cab01c5640c724e7167645ba3230cee10571
Gui Hebling <guilherme.durso@outlook.com>
2020-09-13 20:22:08 +0200

    Finished Checkout Page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Checkout from './Checkout';
 import Login from './Login';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
+import Payment from './Payment';
 
 function App() {
 
@@ -46,6 +47,10 @@ function App() {
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Payment />
           </Route>
 
           {/* Default Route at the bottom:  */}
